c41969972118c8ca872164b9f8e077eb4a6db400
Julio Salas <jscomic@gmail.com>
2023-10-01 20:28:26 -0500

    First animation

---
+++ script.js
@@ -0,0 +1,8 @@
+const element = document.getElementById("element");
+
+function appear() {
+  element.style.opacity = 1;
+}
+
+// Call the appear function after a delay (e.g., 2 seconds)
+setTimeout(appear, 2000);
